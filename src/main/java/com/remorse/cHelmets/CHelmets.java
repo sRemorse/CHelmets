e414ab837480130e2ead388425357da1f920922c
sRemorse <ryanhickeyjersey1@gmail.com>
2026-01-12 18:13:58 +0000

    chore(Main): Cleanup plugin class and initialize managers.
    Additional cleanup and comments added too.

--- src/main/java/com/remorse/cHelmets/CHelmets.java
+++ src/main/java/com/remorse/cHelmets/CHelmets.java
@@ -1,21 +1,34 @@
 package com.remorse.cHelmets;
 
+import com.remorse.cHelmets.config.ConfigManager;
+import com.remorse.cHelmets.cosmetic.CosmeticManager;
 import com.remorse.cHelmets.economy.EconomyHook;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@SuppressWarnings("unused")
 public final class CHelmets extends JavaPlugin {
 
     private EconomyHook economyHook;
+    private ConfigManager configManager;
+    private CosmeticManager cosmeticManager;
+    private MiniMessage miniMessage;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
         this.getLogger().info("Plugin enabled");
 
+        // Init Configs
+        this.configManager = new ConfigManager(this);
+
+        // Init Economy
         this.economyHook = new EconomyHook(this);
 
+        // Init Managers
+        this.cosmeticManager = new CosmeticManager(this);
 
+        // Register commands & Listeners
     }
 
     @Override
@@ -30,7 +43,27 @@ public final class CHelmets extends JavaPlugin {
         });
     }
 
+    private void registerConfigs() {
+        configManager.getDefaultConfig();
+        configManager.get("helmets.yml");
+    }
+
+    // Getters
+
     public EconomyHook getEconomyHook() {
         return economyHook;
     }
+
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+
+    public CosmeticManager getCosmeticManager() {
+        return cosmeticManager;
+    }
+
+    public MiniMessage getMM(){
+        return miniMessage;
+    }
+
 }
