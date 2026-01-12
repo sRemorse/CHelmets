1201ebf7fb588d2e107d332bebb5ae5197c0ce4e
sRemorse <ryanhickeyjersey1@gmail.com>
2026-01-12 15:18:08 +0000

    Initial commit: Creating repo

---
+++ src/main/java/com/remorse/cHelmets/CHelmets.java
@@ -0,0 +1,17 @@
+package com.remorse.cHelmets;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CHelmets extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
